90aafdd285e5712a5a53278c31777619ee00464f
Marcus Winicius Reinisch <marcus.w.reinisch@gmail.com>
2026-04-19 20:50:21 -0400

    finalizado porem com print e gets errados

--- 5. Tecnicas de programacao funcoes e classes/main.js
+++ 5. Tecnicas de programacao funcoes e classes/main.js
@@ -1,19 +1,21 @@
 // IMPORTANTE: As funções "gets" e "print" são acessíveis globalmente, onde:  
 // - "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário; 
 // - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente.
+const {gets, print} = require('./funcoes_auxiliares')
 
 function calcularDesconto(valorCompra) {
     const valor = parseFloat(valorCompra); 
     let descontoPercentual;
 
     // TODO: Verifique o valor da compra e defina o percentual de desconto:
-   
-      // Se o valor for menor que 50, não há desconto
-
+    
+    // Se o valor for menor que 50, não há desconto
+    if (valor < 50.00)  descontoPercentual = 0;
      // Se o valor for entre 50 e 100, o desconto é de 10%
-      
+    else if (valor >= 50.00 && valor <= 100 )  descontoPercentual = 10;
     // Se o valor for superior a 100, o desconto é de 20%
-
+    else if (valor > 100.00)  descontoPercentual = 20;
+    else  console.log("Valor incorreto!!!")
     
 
     return `Desconto de ${descontoPercentual}%`;
