6cafefc61b4f4333f1a8d366d629615ef115041b
Marcus Winicius Reinisch <marcus.w.reinisch@gmail.com>
2026-04-20 21:02:53 -0400

    Saida funcionando

--- 5. Tecnicas de programacao funcoes e classes/funcoes_auxiliares.js
+++ 5. Tecnicas de programacao funcoes e classes/funcoes_auxiliares.js
@@ -1,16 +1,22 @@
-const entradas = [49.99,75.00,150.00];
-
+//// Lista de entradas simuladas (substitua os valores para testar diferentes cenários)
+const entradas = ['49.99','75.00','150.00'];
 let i=0;
 
+/**
+ * Lê a próxima linha do array de entradas.
+ */
 function gets() {
     const valor = entradas[i];
     i++;
-    
-    return valor;
+    return valor !== undefined ? String(valor) : "";
 }
 
+/**
+ * Imprime um valor no console.
+ */
 function print(texto) {
     console.log(texto);
 }
 
+// Exporta as funções para que possam ser usadas em outros arquivos via 'require'
 module.exports = {gets, print}; // exportando um objeto que tem as funcoes get e print
--- 5. Tecnicas de programacao funcoes e classes/main.js
+++ 5. Tecnicas de programacao funcoes e classes/main.js
@@ -10,15 +10,15 @@ function calcularDesconto(valorCompra) {
     // TODO: Verifique o valor da compra e defina o percentual de desconto:
     
     // Se o valor for menor que 50, não há desconto
-    if (valor < 50.00)  descontoPercentual = 0;
-     // Se o valor for entre 50 e 100, o desconto é de 10%
-    else if (valor >= 50.00 && valor <= 100 )  descontoPercentual = 10;
-    // Se o valor for superior a 100, o desconto é de 20%
-    else if (valor > 100.00)  descontoPercentual = 20;
-    else  console.log("Valor incorreto!!!")
+    if (valor < 50.00) {
+     descontoPercentual = 0; 
+    // Se o valor for entre 50 e 100, o desconto é de 10%
+    } else if (valor >= 50.00 && valor <= 100 ) {    // Se o valor for superior a 100, o desconto é de 20%
+       }   else if (valor > 100.00)  descontoPercentual = 20;
+    else  {console.log("Valor incorreto!!!");}
     
 
-    return `Desconto de ${descontoPercentual}%`;
+    return `Valor: R$ ${valor.toFixed(2)} -> Desconto de ${descontoPercentual}%`;
 }
 
 function main() {
